c9f0ca48fcf4c37bbfe04cf244b2282b11888a71
Darragh Tobin <darraghtobin@gmail.com>
2020-10-29 22:34:45 +0000

    a

--- linkedlist.c
+++ linkedlist.c
@@ -12,10 +12,17 @@ return res->next;
 }
 int main() {
     struct node *head = (struct node*)malloc(sizeof(struct node));
+    struct node *node1 = (struct node*)malloc(sizeof(struct node));
+    struct node *node2 = (struct node*)malloc(sizeof(struct node));
     head->value=5;
+    head->next=node1;
+    node1->value=10;
+    node1->next=node2;
+    node2->value=20;
+    node2->value=NULL;
     struct node *temp = head;
-    struct node *node1 = createNode(head,10);
-    struct node *node2 = createNode(node1,20);
+    //struct node *node1 = createNode(head,10);
+    //struct node *node2 = createNode(node1,20);
     while(temp->next){
         temp=temp->next;
         printf("%d",temp->value);}
