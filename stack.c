5ed026b963f5b9d6333162b4a6cbef389e26cbaf
Darragh Tobin <darraghtobin@gmail.com>
2020-11-04 23:54:06 +0000

    stack

---
+++ stack.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define N 10
+struct stack{
+ int top;
+ int array[N];
+};
+
+struct stack* createStack(){
+ struct stack* newStack = malloc(sizeof(struct stack));
+ newStack->top = 0;
+ return newStack;
+}
+
+void push(struct rootStack*,int value){
+ rootStack->array[top]=value;
+ rootStack->top+=1;
+}
+int pop(struct rootStack*){
+ int value = rootStack->array[top-1];
+ rootStack->top-=1;
+ return value;
+}
+
+int main(){
+ struct stack* newStack = createStack();
+ push(newStack,1);
+ push(newStack,2);
+ push(newStack,3);
+ printf("%d ",pop(newStack));
+ printf("%d ",pop(newStack));
+ printf("%d ",pop(newStack));
+}
